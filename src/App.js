41410ce63c7d050aa509db94f767866eb0502725
Jeff Bush <jeffbushdesign@gmail.com>
2023-02-14 16:23:45 -0500

    Practice with destructuring

--- src/App.js
+++ src/App.js
@@ -21,7 +21,27 @@ const employeeInfo = [
   }
 ];
 
+const destructuringExample = {
+  animalType: "Dog",
+  animalColor: "Brown",
+  animalSound: "Woof!"
+};
+
+const contactInfo = {
+  name: "John Doe",
+  address: {
+    street: "Church",
+    country: "Canada",
+    province: "Quebec"
+  }
+};
+
 function App() {
+  const { animalType, animalColor, animalSound } = destructuringExample;
+  const { name, address } = contactInfo;
+  // multi-level deep destructuring
+  // address is first destructured, and then street country and province are further destructured
+  const { street, country, province } = address;
   return (
     <div className="App">
       <header className="App-header">
@@ -33,8 +53,18 @@ function App() {
           return (
             <Employee firstName={firstName} lastName={lastName} age={age} />
           );
-        })
-        }
+        })}
+
+        <h2>Destructuring Example</h2>
+        <h4>{`A ${animalType} is ${animalColor} and goes ${animalSound}`}</h4>
+        <h4>{`${name} lives on ${street} street which is in the province of ${province}, in the country of ${country}`}</h4>
+
+        <h2>Aliasing</h2>
+        <p>If you have the same property twice you can alias one of them by using a colon character. A common example of this would be more than one thing named id. One becomes id: animalId, the other becomes id: humanId.</p>
+
+        <h2>More Complex Use Cases</h2>
+        <h6>They shorten your code but may make it much less readable</h6>
+        <p>Destructuring in place.</p>
 
 
 
