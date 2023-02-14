5f64c59688bd4873935d7dc7c0c67e11c09fa972
Jeff Bush <jeffbushdesign@gmail.com>
2023-02-14 16:49:43 -0500

    Mapping through objects

--- src/App.js
+++ src/App.js
@@ -36,6 +36,29 @@ const contactInfo = {
   }
 };
 
+const people = [
+  "Mark",
+  "Jeff",
+  "Trevor"
+];
+
+const VRGames = [
+  {
+    title: "Saints & Sinners",
+    rating: "AAA"
+  },
+  {
+    title: "Retribution",
+    rating: "B"
+  },
+  {
+    title: "Office Simulator",
+    rating: "A"
+  }
+];
+
+
+
 function App() {
   const { animalType, animalColor, animalSound } = destructuringExample;
   const { name, address } = contactInfo;
@@ -44,35 +67,46 @@ function App() {
   const { street, country, province } = address;
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Welcome to my counter</h2>
-        <Counter />
-        <h2> Company Directory </h2>
-        {employeeInfo.map((employee) => {
-          const { firstName, lastName, age } = employee;
-          return (
-            <Employee firstName={firstName} lastName={lastName} age={age} />
-          );
-        })}
+      <h2>Welcome to my counter</h2>
+      <Counter />
+      <h2> Company Directory </h2>
+      {employeeInfo.map((employee) => {
+        const { firstName, lastName, age } = employee;
+        return (
+          <Employee firstName={firstName} lastName={lastName} age={age} />
+        );
+      })}
 
-        <h2>Destructuring Example</h2>
-        <h4>{`A ${animalType} is ${animalColor} and goes ${animalSound}`}</h4>
-        <h4>{`${name} lives on ${street} street which is in the province of ${province}, in the country of ${country}`}</h4>
+      <h2>Destructuring Example</h2>
+      <h4>{`A ${animalType} is ${animalColor} and goes ${animalSound}`}</h4>
+      <h4>{`${name} lives on ${street} street which is in the province of ${province}, in the country of ${country}`}</h4>
 
-        <h2>Aliasing</h2>
-        <p>If you have the same property twice you can alias one of them by using a colon character. A common example of this would be more than one thing named id. One becomes id: animalId, the other becomes id: humanId.</p>
+      <h2>Aliasing</h2>
+      <p>If you have the same property twice you can alias one of them by using a colon character. A common example of this would be more than one thing named id. One becomes id: animalId, the other becomes id: humanId.</p>
 
-        <h2>More Complex Use Cases</h2>
-        <h6>They shorten your code but may make it much less readable</h6>
-        <p>Destructuring in place.</p>
+      <h2>More Complex Use Cases</h2>
+      <h6>They shorten your code but may make it much less readable</h6>
+      <p>Destructuring in place.</p>
+
+      <h2>Mapping & Looping</h2>
+      {people.map((people) => {
+        return <h3>{people}</h3>;
+      })}
+      <p>
+        If all your function is doing is returning something then you can omit the return statement and the curly braces.
+      </p>
+      {people.map((people) => (
+        <h4>{people}</h4>
+      ))}
+
+      <p>
+        Now an array of objects instead of just an array of strings...
+      </p>
+      {people.map((people) => (
+        <h4>{people}</h4>
+      ))}
 
 
-
-        {/* <Employee firstName="Jack" lastName="Smith" />
-        <Employee firstName="Stef" lastName="Cole" />
-        <Employee firstName="George" lastName="Plant" />
-        <Employee firstName="Tom" lastName="Jones" /> */}
-      </header>
     </div>
   );
 }
